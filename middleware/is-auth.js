41dec1eec3f6be1fe60c069f4b03e12752e041f2
ronodhirSoumik <ronodhirsoumik@gmail.com>
2021-05-12 22:45:14 +0600

    add keyword and file encryption

--- middleware/is-auth.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {  // isLoggedIn was set on authController/postLogin route.
-        return res.redirect('/login'); // If No loggedIn user found , then promt user to log in first.
-    }
-    next();
-}
