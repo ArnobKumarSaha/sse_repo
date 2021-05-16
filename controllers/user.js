3ea8b7c34a2458fe425c89e80472d4403267a9c9
Arnob kumar saha <arnobkumarsaha00@gmail.com>
2021-05-16 22:48:28 +0600

    sequence matters man

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,7 @@ exports.postUploadFile = (req, res, next) =>{
   let space_separated_keywords = keyword.split(' ');
 
 
-  console.log("keywords in userController postUploadFile() = ", space_separated_keywords);
+  //console.log("keywords in userController postUploadFile() = ", space_separated_keywords);
 
   space_separated_keywords.forEach(tmpKey =>{
     /*console.log("keykey", tmpKey, typeof(tmpKey));
@@ -45,12 +45,6 @@ exports.postUploadFile = (req, res, next) =>{
     console.log(typeof(KeywordIndex.addThisKeywordIndex));*/
 
     const keyHash = encDec.getKeywordHash(tmpKey);
-
-    /*
-    const temp = this.index_hash.filter(keyHash => {
-      return this.index_hash == keyHash;
-    });
-    */
 
     KeywordIndex.findOne({index_hash: keyHash}).then(keyDoc => {
       if(keyDoc){
@@ -96,8 +90,115 @@ exports.getDownloadFile = (req, res, next) =>{
   });
 }
 
-exports.postDownloadFile = (req, res, next) =>{
-  res.send("Into post Download File route.");
+var freqTable = {};
+
+function calculate1(space_separated_keywords){
+
+  return new Promise( (resolve, reject) => {
+
+      console.log("calculate Function starts. ", space_separated_keywords);
+
+      for(let tmpKey of space_separated_keywords){
+        const keyHash = encDec.getKeywordHash(tmpKey);
+        console.log(tmpKey, keyHash);
+
+        KeywordIndex.findOne({index_hash: keyHash})
+        .then(keyDoc => {
+          console.log("keyDoc = ", keyDoc);
+          if(keyDoc){
+            let myfiles = [...keyDoc.whereItIs.myFiles];
+
+            for(var fl of myfiles){
+              var fp = fl.filePath;
+              console.log("Type = ", typeof(fp));
+
+              if(freqTable[fp]) {freqTable[fp]++; console.log("IN true.");}
+              else { freqTable[fp] = 1; console.log("In False. ", freqTable);}
+            }
+            
+          }
+          else{
+            console.log("Inside else block!");
+          }
+        });
+      }
+      resolve();
+   });
+}
+var sortable = [];
+function calculate2(){
+        //setTimeout(()=>{
+        // Now , sort them in descending order to get top 5.
+      return new Promise( (resolve, reject) =>{
+        
+        for (var file in freqTable) {
+            sortable.push([file, freqTable[file]]);
+        }
+
+        console.log("Printing sortable 1: ", sortable);
+
+        sortable.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        console.log("Printing sortable 2: ", sortable);
+
+        // This is to show the Owner of the top-matched files.
+
+        /*User.findOne({ "cart.myFiles.filePath": fp},  {name: 1} ).then(userDoc => {
+          console.log(userDoc.name);
+        })
+          .catch(err => console.log(err));*/
+        
+
+        console.log("Calculate function ends. (Before resolve) ", freqTable, typeof(freqTable) );
+        resolve(sortable);
+        console.log("Calculate function ends. (After resolve) ", freqTable, typeof(freqTable) );
+        //return sortable;
+      });
+      //}, 3000);
+}
+
+async function intermediateFunction(space_separated_keywords){
+  console.log("At the start of intermediate function.");
+  //const documents = await calculate1(space_separated_keywords);
+  /*calculate1(space_separated_keywords).then(() =>{
+    console.log("Here I am !");
+    calculate2().then(documents =>{
+      console.log("Before the return statement of intermediate function. ", documents);
+      return documents;
+    });
+  })*/
+  await calculate1(space_separated_keywords);
+  await calculate2();
+  console.log("Before return , and after calculates called, in intermediate");
+  return;
+}
+
+exports.postDownloadFile = async (req, res, next) =>{
+
+  const keyword = req.body.keyword;
+
+  let space_separated_keywords = keyword.split(' ');
+
+
+  /*intermediateFunction(space_separated_keywords)
+    .then( freqTable => {
+      console.log("keywords in userController postDownloadFile() = ", space_separated_keywords);
+      console.log(freqTable);
+    
+      res.send("Into post Download File route.");
+    })
+    .catch(err => console.log(err) );*/
+
+  await intermediateFunction(space_separated_keywords);
+  console.log("In postDownloadFunction ", sortable);
+  
+  res.render('updown/searchResult', {
+    pageTitle: "Search result",
+    path: "/user/searchResult",
+    docs: sortable
+  });
 }
 
 
